a51fd87e30b1bd4fc7af4d6b56b7653a16182a48
Sourabh Kumar <inspiron3593_8MT6S53@outlook.com>
2022-09-03 23:34:30 +0530

    start

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          itzsourabhkumar!
+           Let's get started!
         </p>
         <a
           className="App-link"
